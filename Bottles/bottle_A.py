cab6834361da485e8f64d424c7878cff8f403a46
572470467 <41976463+572470467@users.noreply.github.com>
2019-12-27 11:54:37 +0800

    Update bottle_A.py

--- Bottles/bottle_A.py
+++ Bottles/bottle_A.py
@@ -4,6 +4,7 @@ from tkinter import*
 import threading
 import json
 import os
+import sys
 import socket
 import csv ,sys,operator
 import tkinter.filedialog
@@ -17,24 +18,18 @@ os.environ['SDL_VIDEO_WINDOW_POS']= "%d,%d" % (67,27)
 state='closey'
 state_a=['close','close','close','close','close']
 state_b=['close','close','close','close']
-line=['http://192.168.10.200:5000/measure/{0}/{1}','http://192.168.10.201:5000/measure/{0}/{1}']
 Brack=[0,0,0]
 White=[255,255,255]
 Red=[255,0,0]
 Green=[0,255,0]
 Gray=[169,169,169]
-width=1360
-height=768
-name='  OPEN'
-#width=pygame.display.Info().current_w
-#height=pygame.display.Info().current_h
-height0=int((height-117)/3)
+width=pygame.display.Info().current_w
+height=pygame.display.Info().current_h
 size0=(width-67)/9
-color0=[Green,Green,Green]
+color0=[Green,Green,Green,Green]
 number_a=["A0","A1","A2","A3","A4"]
 number_b=["B0","B1","B2","B3"]
-icon={'00':'kz.jpg','10':'bz.jpg','11':'mz.jpg','01':'gz.jpg'}
-#screen = pygame.display.set_mode((width-67,height0))
+icon={'11':'kz.jpg','01':'bz.jpg','00':'mz.jpg','10':'gz.jpg'}
 screen = pygame.display.set_mode((int((width-67)/3),height))
 text=pygame.font.SysFont("arial",24)
 text1=pygame.font.SysFont("arial",16)
@@ -73,8 +68,7 @@ class Bottles(object):
     def PF():
         root=tkinter.Tk()
         root.withdraw()
-        default_dir= r"C:\Users\lenovo\Desktop\Bottles"  
-        fname= tkinter.filedialog.askopenfilename(title=u"选择配方",initialdir=(os.path.expanduser(default_dir)))
+        fname= tkinter.filedialog.askopenfilename(title=u"Select formula",initialdir=num)
         f=open(fname)
         reader=csv.reader(f)
         lt=[]
@@ -83,8 +77,8 @@ class Bottles(object):
         for t in lt:
             root=tkinter.Tk()
             root.withdraw()
-            response=urllib.request.urlopen(line[num].format(t[0],t[1]))
-            print(line[num].format(t[0],t[1]))
+            response=urllib.request.urlopen('http://192.168.10.200:5000/measure/{0}/{1}'.format(t[0],t[1]))
+            print('http://192.168.10.200:5000/measure/{0}/{1}'.format(t[0],t[1]))
         color0[n]=Green
         mainloop()
     def State(num,n):   
@@ -111,55 +105,58 @@ if __name__ == '__main__':
                                 if t[3]=='FT_lifter' or t[3]=='FT_base':
                                     if color0[t[0]]==Green:
                                         color0[t[0]]=Red
-                                        num=0
+                                        num=t[3]
                                         n=t[0]
                                         Bottles.State(num,n)
+                                elif t[3]=='Reelect':
+                                    python=sys.executable
+                                    os.execl(python,python,*sys.argv)
                                 elif t[0]==2:
                                     if color0[t[0]]==Green:
                                         color0[t[0]]=Red
                                         name=' CLOSE'
-                                        #response=urllib.request.urlopen("http://192.168.10.200:5000/valve/1")          
+                                        response=urllib.request.urlopen("http://192.168.10.200:5000/valve/1")             
                                     elif color0[t[0]]==Red:
                                         color0[t[0]]=Green
                                         name='  OPEN'
-                                        #response=urllib.request.urlopen("http://192.168.10.200:5000/valve/0")          
-                        for i in B0:
-                            if i[1]<=pos[0]<=i[1]+90 and i[2]<=pos[1]<=i[2]+90:
-                                if state_a==['close','close','close','close','close']:
-                                    if index ==0:
-                                        state_a[i[0]]='open'
+                                        response=urllib.request.urlopen("http://192.168.10.200:5000/valve/0")
+                        #for i in B0:
+                        #    if i[1]<=pos[0]<=i[1]+90 and i[2]<=pos[1]<=i[2]+90:
+                        #        if state_a==['close','close','close','close','close']:
+                        #            if index ==0:
+                        #                state_a[i[0]]='open'
                                         #response0=urllib.request.urlopen('http://192.168.10.200:5000/feeder/{}/1'.format(i[0]))
                                         #html0=response0.read()
                                         #text0=json.loads(html0)
-                                        print('http://192.168.10.200:5000/feeder/{}/1'.format(i[0]))
-                                elif state_a[i[0]]=='open':
-                                    if index ==2:
-                                        state_a[i[0]]='close'
+                        #                print('http://192.168.10.200:5000/feeder/{}/1'.format(i[0]))
+                        #        elif state_a[i[0]]=='open':
+                        #            if index ==2:
+                        #                state_a[i[0]]='close'
                                         #response1=urllib.request.urlopen('http://192.168.10.200:5000/feeder/{}/0'.format(i[0]))
                                         #html1=response1.read()
                                         #text1=json.loads(html1)
-                                        print('http://192.168.10.200:5000/feeder/{}/0'.format(i[0]))
-                        if size0<=pos[0]<=size0+90 and 370<=pos[1]<=460:
-                            if index == 0:
-                                response4=urllib.request.urlopen("http://192.168.10.200:5000/feederon")
-                                html4=response4.read()
-                                text4=json.loads(html4)
-                            elif index==2:
-                                response5=urllib.request.urlopen("http://192.168.10.200:5000/feederoff")
-                                html5=response5.read()
-                                text5=json.loads(html5)
+                        #                print('http://192.168.10.200:5000/feeder/{}/0'.format(i[0]))
+                        #if size0<=pos[0]<=size0+90 and 370<=pos[1]<=460:
+                        #    if index == 0:
+                        #        response4=urllib.request.urlopen("http://localhost:80/feederon/a")
+                        #        html4=response4.read()
+                        #        text4=json.loads(html4)
+                        #    elif index==2:
+                        #        response5=urllib.request.urlopen("http://localhost:80/feederoff/a")
+                        #        html5=response5.read()
+                        #        text5=json.loads(html5)
         time.sleep(1/3)
-        response8=urllib.request.urlopen("http://192.168.10.119:80/bucketgroup/a")
+        response8=urllib.request.urlopen("http://192.168.10.200:5000/level")
         html8=response8.read().decode()
         response10=urllib.request.urlopen("http://192.168.10.200:5000/scale")
         html10=response10.read()
         text8=json.loads(html8)
         text10=json.loads(html10)
-        B0=[[0,70,160,text8['0']],[1,70,30,text8['1']],[2,size0*2+20,240,text8['2']],[3,size0*2+20,120,text8['3']],[4,size0*2+20,0,text8['4']]]
-        list=[[0,size0*3-100,370,'FT_lifter'],[1,size0*3-100,490,'FT_base'],[2,size0+10,640,name]]
+        B0=[[0,70,160,str(text8[0][0])+str(text8[1][0])],[1,70,30,str(text8[0][1])+str(text8[1][1])],[2,size0*2+20,240,str(text8[0][2])+str(text8[1][2])],[3,size0*2+20,120,str(text8[0][3])+str(text8[1][3])],[4,size0*2+20,0,str(text8[0][4])+str(text8[1][4])]]
+        list=[[0,size0*3-100,370,'FT_lifter'],[1,size0*3-100,490,'FT_base'],[2,size0+10,640,name],[3,size0*3-100,610,'Reelect']]
         for t in list:
             Bottles.AN(t[0],t[1],t[2],t[3])
         for i in B0:
             Bottles.Icon_a(i[0],i[1],i[2],i[3])
         Bottles.VibratoryFeeder(size0,370,'lx.jpg')
-        Bottles.WeightIcon(size0,490,b'%0.2f' % text10)
+        Bottles.WeightIcon(size0,490,b'%0.2f' % text10['reading'])
