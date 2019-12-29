1fbd0002d6ca38d3bd2b866f298f55e79ed0a3bd
572470467 <41976463+572470467@users.noreply.github.com>
2019-12-29 10:24:15 +0800

    Update interface.py

--- Bottles/interface.py
+++ Bottles/interface.py
@@ -15,25 +15,25 @@ Gray=[169,169,169]
 width=pygame.display.Info().current_w
 height=pygame.display.Info().current_h
 height0=int((height-117)/3)
-size=int((width-400)/2)
-button_text=["开 始","开 始","开 始","开 始","开 始","开 始","开 始"]
+size=((width-67)/3-400)/2
+button_text=["begin","begin","begin","begin","begin","begin","begin"]
 line=['http://192.168.10.119:80/mixer/000','http://192.168.10.119:80/mixer/100','http://192.168.10.119:80/mixer/200','http://192.168.10.119:80/mixer/300','http://192.168.10.119:80/mixer/400','http://192.168.10.119:80/mixer/500','http://192.168.10.119:80/mixer/600']
-line0=['http://192.168.10.119:80/carrier/moveto/0','http://192.168.10.119:80/carrier/moveto/1','http://192.168.10.119:80/carrier/moveto/2','http://192.168.10.119:80/carrier/moveto/3','http://192.168.10.119:80/carrier/moveto/4']
+line0=['http://192.168.10.108:5000/carrier/moveto/0','http://192.168.10.108:5000/carrier/moveto/1','http://192.168.10.108:5000/carrier/moveto/2','http://192.168.10.108:5000/carrier/moveto/3','http://192.168.10.108:5000/carrier/moveto/4']
 CGQ=[[0,1,1,1,1],[1,0,1,1,1],[1,1,0,1,1],[1,1,1,0,1],[1,1,1,1,0]]
 color=[Green,Green,Green,Green,Green,Green,Green]
 color0=[Green,Green,Green,Green,Green]
-button_text0="手动状态:"
-button_text1=["工位0","工位1","工位2","工位3","工位4"]
+button_text0="manual:"
+button_text1=["station0","station1","station2","station3","station4"]
 Num=['0','1','2','3','4']
 B0=[size+32,size+102,size+172,size+242,size+312]
-os.environ['SDL_VIDEO_WINDOW_POS']= "%d,%d" % (67,height0+57)
-#screen = pygame.display.set_mode((width-67,height0))
-screen = pygame.display.set_mode((width-67,620))
+os.environ['SDL_VIDEO_WINDOW_POS']= "%d,%d" % (67+(width-67)/3*2,27)
+screen = pygame.display.set_mode((int((width-67)/3),height))
 screen.fill(Brack)
-text=["接        粉        体:","粉     体     混     合:","粉 体 倒 下 锅 混 合:","制  作  润  湿  剂:","磨   料   混   合:","润 湿 剂 泵 进 磨 料:","磨料下锅混合后倒入小车:"]
-text_0=pygame.font.Font("/usr/share/fonts/truetype/wqy/wqy-zenhei.ttc",24)
-text_1=pygame.font.Font("/usr/share/fonts/truetype/wqy/wqy-zenhei.ttc",20)
-text_fmt0=text_0.render("操     作     界     面",2,White)
+text=["After  powder:","Powder  mixed:","Pour and mix the powder:","Make  lubricant:","Mixed  abrasive:","lubricant into abrasive:","pour mixture into car:"]
+text_0=pygame.font.SysFont("arial",24)
+text_1=pygame.font.SysFont("arial",20)
+text_2=pygame.font.SysFont("arial",17)
+text_fmt0=text_0.render("Operation  Interface",2,White)
 screen.blit(text_fmt0,(size+106,10))
 pygame.display.update()
 def Process(num,x,y,button_text,color):
@@ -45,28 +45,28 @@ def Process(num,x,y,button_text,color):
     pygame.display.update()
 def Station(num,x,y,a,color0):
     pygame.draw.rect(screen,color0,[x,y,65,28],0)
-    button=text_1.render(button_text1[num],1,Brack)
-    screen.blit(button,(x+9,y+4))
+    button=text_2.render(button_text1[num],1,Brack)
+    screen.blit(button,(x+2,y+4))
     img=pygame.image.load('cgq.jpg')
     img=pygame.transform.smoothscale(img,(52,50))
     screen.blit(img,(x,y+80))
-    button=text_1.render((Num[a]),1,White)
+    button=text_0.render((Num[a]),1,White)
     screen.blit(button,(x+20,556))
     pygame.display.update()
 def State_A():
     response=urllib.request.urlopen(line[num])
-    button_text[num]="开 始"
+    button_text[num]="begin"
     color[num]=Green
 def State_B():
     for i in range(6):
         response=urllib.request.urlopen(line[i])
-        button_text[i]="开 始"
-        button_text[i+1]="结 束"
+        button_text[i]="begin"
+        button_text[i+1]="end"
     response=urllib.request.urlopen(line[6])
-    button_text[6]="开 始"
+    button_text[6]="begin"
 def State_C(num):
     pygame.draw.rect(screen,Brack,[size,461,400,50],0)
-    pygame.draw.rect(screen,Brack,[size,551,400,25],0)
+    pygame.draw.rect(screen,Brack,[size,555,400,25],0)
     img=pygame.image.load('car.jpg')
     img=pygame.transform.smoothscale(img,(52,50))
     screen.blit(img,(B0[num],461))
@@ -92,21 +92,21 @@ def Car(num):
 if __name__ == '__main__':
     while True:
         time.sleep(1.5)
-        pygame.draw.rect(screen,Brack,[size+86,380,85,28],0)
+        pygame.draw.rect(screen,Brack,[size+70,380,100,28],0)
         pygame.draw.rect(screen,Green,[size+177,382,62,25],0)
-        button1=text_1.render("切 换",1,Brack)
-        screen.blit(button1,(size+191,384))
+        button1=text_1.render("switch",1,Brack)
+        screen.blit(button1,(size+181,384))
         button=text_1.render(button_text0,1,White)
-        screen.blit(button,(size+86,384))
+        screen.blit(button,(size+70,384))
         B=[[0,size+187,66,button_text[0],color[0]],[1,size+187,112,button_text[1],color[1]],[2,size+187,158,button_text[2],color[2]],[3,size+187,204,button_text[3],color[3]],[4,size+187,250,button_text[4],color[4]],[5,size+187,296,button_text[5],color[5]],[6,size+187,342,button_text[6],color[6]]]
         for i in B:
             Process(i[0],i[1],i[2],i[3],i[4])
         if color0==[Green,Green,Green,Green,Green]:
-            if button_text==["开 始","开 始","开 始","开 始","开 始","开 始","开 始"]:
-                response2=urllib.request.urlopen('http://192.168.10.119:80/carrier/status')
+            if button_text==["begin","begin","begin","begin","begin","begin","begin"]:
+                response2=urllib.request.urlopen('http://192.168.10.108:5000/carrier/status')
                 html2=response2.read()
                 text2=json.loads(html2)
-            elif button_text!=["开 始","开 始","开 始","开 始","开 始","开 始","开 始"]:
+            elif button_text!=["begin","begin","begin","begin","begin","begin","begin"]:
                 text2={'pos': 0, 'sensors': [0, 1, 1, 1, 1]}
             a=text2['sensors']
             b=text2['pos']
@@ -127,21 +127,21 @@ if __name__ == '__main__':
                     if pressed_array[index]:
                         if index==0:
                             if size+177<=pos[0]<=size+242 and 376<=pos[1]<=404:
-                                if button_text0=="自动状态:" and button_text==["开 始","开 始","开 始","开 始","开 始","开 始","开 始"]:
-                                    button_text0="手动状态:"
+                                if button_text0=="self-motion:" and button_text==["begin","begin","begin","begin","begin","begin","begin"]:
+                                    button_text0="manual:"
                                     color=[Green,Green,Green,Green,Green,Green,Green]
-                                elif button_text0=="手动状态:" and button_text==["开 始","开 始","开 始","开 始","开 始","开 始","开 始"]:
-                                    button_text0="自动状态:"
-                                    button_text[0]="结 束"
+                                elif button_text0=="manual:" and button_text==["begin","begin","begin","begin","begin","begin","begin"]:
+                                    button_text0="self-motion:"
+                                    button_text[0]="end"
                                     Automatic()
                                     color=[Gray,Gray,Gray,Gray,Gray,Gray,Gray]
                             for i in B:
                                 if i[1]+100<=pos[0]<=i[1]+165 and i[2]<=pos[1]<=i[2]+28:
-                                    if button_text==["开 始","开 始","开 始","开 始","开 始","开 始","开 始"] and button_text0=="手动状态:":
+                                    if button_text==["begin","begin","begin","begin","begin","begin","begin"] and button_text0=="manual:":
                                         color[i[0]]=Red
                                         num=i[0]
-                                        button_text[i[0]]="结 束"
-                                        if button_text[i[0]]=="结 束":
+                                        button_text[i[0]]="end"
+                                        if button_text[i[0]]=="end":
                                             Manual(num)
                             for v in C:
                                 if v[1]<=pos[0]<=v[1]+65 and v[2]<=pos[1]<=v[2]+28 and color0==[Green,Green,Green,Green,Green]:
