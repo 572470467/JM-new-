4c4d46b4d219b19a150dedba373736551211b07c
572470467 <41976463+572470467@users.noreply.github.com>
2020-01-02 13:34:08 +0800

    Update interface.py

--- Bottles/interface.py
+++ Bottles/interface.py
@@ -16,11 +16,11 @@ width=pygame.display.Info().current_w
 height=pygame.display.Info().current_h
 height0=int((height-117)/3)
 size=((width-67)/3-400)/2
-button_text=["begin","begin","begin","begin","begin","begin","begin"]
-line=['http://192.168.10.117:5000/mixer/000','http://192.168.10.117:5000/mixer/100','http://192.168.10.117:5000/mixer/200','http://192.168.10.117:5000/mixer/300','http://192.168.10.117:5000/mixer/400','http://192.168.10.117:5000/mixer/500','http://192.168.10.117:5000/mixer/600']
-line0=['http://192.168.10.108:5000/carrier/moveto/0','http://192.168.10.108:5000/carrier/moveto/1','http://192.168.10.108:5000/carrier/moveto/2','http://192.168.10.108:5000/carrier/moveto/3','http://192.168.10.108:5000/carrier/moveto/4']
+button_text=["begin","begin","begin","begin","begin","begin","begin","begin","begin"]
+line=['http://192.168.10.117:5000/mixer/000','http://192.168.10.117:5000/mixer/100','http://192.168.10.117:5000/mixer/200','http://192.168.10.117:5000/mixer/300','http://192.168.10.117:5000/mixer/400','http://192.168.10.117:5000/mixer/500','http://192.168.10.117:5000/mixer/600','http://192.168.10.117:5000/mixer/700','http://192.168.10.117:5000/mixer/800']
+line0=['http://192.168.10.108:5000/carrier/moveto/0','http://192.168.10.108::5000/carrier/moveto/1','http://192.168.10.108:5000/carrier/moveto/2','http://192.168.10.108:5000/carrier/moveto/3','http://192.168.10.108:5000/carrier/moveto/4']
 CGQ=[[0,1,1,1,1],[1,0,1,1,1],[1,1,0,1,1],[1,1,1,0,1],[1,1,1,1,0]]
-color=[Green,Green,Green,Green,Green,Green,Green]
+color=[Green,Green,Green,Green,Green,Green,Green,Green,Green]
 color0=[Green,Green,Green,Green,Green]
 button_text0="manual:"
 button_text1=["station0","station1","station2","station3","station4"]
@@ -29,7 +29,7 @@ B0=[size+32,size+102,size+172,size+242,size+312]
 os.environ['SDL_VIDEO_WINDOW_POS']= "%d,%d" % (67+(width-67)/3*2,27)
 screen = pygame.display.set_mode((int((width-67)/3),height))
 screen.fill(Brack)
-text=["After  powder:","Powder  mixed:","Pour and mix the powder:","Make  lubricant:","Mixed  abrasive:","lubricant into abrasive:","pour mixture into car:"]
+text=["Power preparation:","Device reset:","Power tank open:","Power tank close:","Circular screen:","Stop and reset:","Grinding machine open:","Grinding machine close:","Order to test:"]
 text_0=pygame.font.SysFont("arial",24)
 text_1=pygame.font.SysFont("arial",20)
 text_2=pygame.font.SysFont("arial",17)
@@ -51,7 +51,7 @@ def Station(num,x,y,a,color0):
     img=pygame.transform.smoothscale(img,(52,50))
     screen.blit(img,(x,y+80))
     button=text_0.render((Num[a]),1,White)
-    screen.blit(button,(x+20,556))
+    screen.blit(button,(x+20,688))
     pygame.display.update()
 def State_A():
     response=urllib.request.urlopen(line[num])
@@ -65,11 +65,11 @@ def State_B():
     response=urllib.request.urlopen(line[6])
     button_text[6]="begin"
 def State_C(num):
-    pygame.draw.rect(screen,Brack,[size,461,400,50],0)
-    pygame.draw.rect(screen,Brack,[size,555,400,25],0)
+    pygame.draw.rect(screen,Brack,[size,593,400,50],0)
+    pygame.draw.rect(screen,Brack,[size,687,400,25],0)
     img=pygame.image.load('car.jpg')
     img=pygame.transform.smoothscale(img,(52,50))
-    screen.blit(img,(B0[num],461))
+    screen.blit(img,(B0[num],593))
     pygame.display.update()
 def State_D():
     response3=urllib.request.urlopen(line0[num])
@@ -92,22 +92,22 @@ def Car(num):
 if __name__ == '__main__':
     while True:
         time.sleep(1.5)
-        pygame.draw.rect(screen,Brack,[size+70,380,100,28],0)
-        pygame.draw.rect(screen,Green,[size+177,382,62,25],0)
+        pygame.draw.rect(screen,Brack,[size+70,482,100,28],0)
+        pygame.draw.rect(screen,Green,[size+177,484,62,25],0)
         button1=text_1.render("switch",1,Brack)
-        screen.blit(button1,(size+181,384))
+        screen.blit(button1,(size+181,486))
         button=text_1.render(button_text0,1,White)
-        screen.blit(button,(size+70,384))
-        B=[[0,size+187,66,button_text[0],color[0]],[1,size+187,112,button_text[1],color[1]],[2,size+187,158,button_text[2],color[2]],[3,size+187,204,button_text[3],color[3]],[4,size+187,250,button_text[4],color[4]],[5,size+187,296,button_text[5],color[5]],[6,size+187,342,button_text[6],color[6]]]
+        screen.blit(button,(size+70,486))
+        B=[[0,size+187,66,button_text[0],color[0]],[1,size+187,112,button_text[1],color[1]],[2,size+187,158,button_text[2],color[2]],[3,size+187,204,button_text[3],color[3]],[4,size+187,250,button_text[4],color[4]],[5,size+187,296,button_text[5],color[5]],[6,size+187,342,button_text[6],color[6]],[7,size+187,388,button_text[7],color[7]],[8,size+187,434,button_text[8],color[8]]]
         for i in B:
             Process(i[0],i[1],i[2],i[3],i[4])
         if color0==[Green,Green,Green,Green,Green]:
-            if button_text==["begin","begin","begin","begin","begin","begin","begin"]:
+            if button_text==["begin","begin","begin","begin","begin","begin","begin","begin","begin"]:
                 response2=urllib.request.urlopen('http://192.168.10.108:5000/carrier/status')
                 html2=response2.read().decode()
-            elif button_text!=["begin","begin","begin","begin","begin","begin","begin"]:
-                html2={'pos': 0, 'sensors': [0, 1, 1, 1, 1]}
-            C=[[0,size+32,426,int(html2[23]),color0[0]],[1,size+102,426,int(html2[26]),color0[1]],[2,size+172,426,int(html2[29]),color0[2]],[3,size+242,426,int(html2[32]),color0[3]],[4,size+312,426,int(html2[35]),color0[4]]]
+            elif button_text!=["begin","begin","begin","begin","begin","begin","begin","begin","begin"]:
+                html2=str({'pos':0,'sensors':[0,1,1,1,1]})
+            C=[[0,size+32,558,int(html2[23]),color0[0]],[1,size+102,558,int(html2[26]),color0[1]],[2,size+172,558,int(html2[29]),color0[2]],[3,size+242,558,int(html2[32]),color0[3]],[4,size+312,558,int(html2[35]),color0[4]]]
             State_C(int(html2[8]))
             for v in C:
                 Station(v[0],v[1],v[2],v[3],v[4])
@@ -123,18 +123,18 @@ if __name__ == '__main__':
                 for index in range(len(pressed_array)):
                     if pressed_array[index]:
                         if index==0:
-                            if size+177<=pos[0]<=size+242 and 376<=pos[1]<=404:
-                                if button_text0=="self-motion:" and button_text==["begin","begin","begin","begin","begin","begin","begin"]:
+                            if size+177<=pos[0]<=size+242 and 484<=pos[1]<=509:
+                                if button_text0=="self-motion:" and button_text==["begin","begin","begin","begin","begin","begin","begin","begin","begin"]:
                                     button_text0="manual:"
-                                    color=[Green,Green,Green,Green,Green,Green,Green]
-                                elif button_text0=="manual:" and button_text==["begin","begin","begin","begin","begin","begin","begin"]:
+                                    color=[Green,Green,Green,Green,Green,Green,Green,Green,Green]
+                                elif button_text0=="manual:" and button_text==["begin","begin","begin","begin","begin","begin","begin","begin","begin"]:
                                     button_text0="self-motion:"
                                     button_text[0]="end"
                                     Automatic()
-                                    color=[Gray,Gray,Gray,Gray,Gray,Gray,Gray]
+                                    color=[Gray,Gray,Gray,Gray,Gray,Gray,Gray,Gray,Gray]
                             for i in B:
                                 if i[1]+100<=pos[0]<=i[1]+165 and i[2]<=pos[1]<=i[2]+28:
-                                    if button_text==["begin","begin","begin","begin","begin","begin","begin"] and button_text0=="manual:":
+                                    if button_text==["begin","begin","begin","begin","begin","begin","begin","begin","begin"] and button_text0=="manual:":
                                         color[i[0]]=Red
                                         num=i[0]
                                         button_text[i[0]]="end"
@@ -147,7 +147,7 @@ if __name__ == '__main__':
                                         num=v[0]
                                         State_C(num)
                                         Car(num)
-                                        C=[[0,size+32,426,CGQ[v[0]][0],color0[0]],[1,size+102,426,CGQ[v[0]][1],color0[1]],[2,size+172,426,CGQ[v[0]][2],color0[2]],[3,size+242,426,CGQ[v[0]][3],color0[3]],[4,size+312,426,CGQ[v[0]][4],color0[4]]]
+                                        C=[[0,size+32,558,CGQ[v[0]][0],color0[0]],[1,size+102,558,CGQ[v[0]][1],color0[1]],[2,size+172,558,CGQ[v[0]][2],color0[2]],[3,size+242,558,CGQ[v[0]][3],color0[3]],[4,size+312,558,CGQ[v[0]][4],color0[4]]]
                                         for f in C:
                                             Station(f[0],f[1],f[2],f[3],f[4])
     pygame.display.update()
