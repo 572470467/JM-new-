6386ac638ccb942b266632fefb090315d7555bfb
572470467 <41976463+572470467@users.noreply.github.com>
2020-01-10 12:44:04 +0800

    Update interface.py

--- Bottles/interface.py
+++ Bottles/interface.py
@@ -16,6 +16,7 @@ width=pygame.display.Info().current_w
 height=pygame.display.Info().current_h
 height0=int((height-117)/3)
 size=((width-67)/3-400)/2
+text1=["mixer recption","circular screen","mixer relrase"]
 button_text=["begin","begin","begin","begin","begin","begin","begin","begin","begin"]
 line=['http://192.168.10.117:5000/mixer/000','http://192.168.10.117:5000/mixer/100','http://192.168.10.117:5000/mixer/200','http://192.168.10.117:5000/mixer/300','http://192.168.10.117:5000/mixer/400','http://192.168.10.117:5000/mixer/500','http://192.168.10.117:5000/mixer/600','http://192.168.10.117:5000/mixer/700','http://192.168.10.117:5000/mixer/800']
 line0=['http://192.168.10.108:5000/carrier/moveto/0','http://192.168.10.108:5000/carrier/moveto/1','http://192.168.10.108:5000/carrier/moveto/2','http://192.168.10.108:5000/carrier/moveto/3','http://192.168.10.108:5000/carrier/moveto/4']
@@ -52,7 +53,7 @@ def Station(num,x,y,a,color0):
     img=pygame.transform.smoothscale(img,(52,50))
     screen.blit(img,(x,y+80))
     button=text_0.render((Num[a]),1,White)
-    screen.blit(button,(x+20,688))
+    screen.blit(button,(x+20,668))
     pygame.display.update()
 def State_A():
     response=urllib.request.urlopen(line[num])
@@ -66,11 +67,11 @@ def State_B():
     response=urllib.request.urlopen(line[6])
     button_text[6]="begin"
 def State_C(num):
-    pygame.draw.rect(screen,Brack,[size,593,400,50],0)
-    pygame.draw.rect(screen,Brack,[size,687,400,25],0)
+    pygame.draw.rect(screen,Brack,[size,573,400,50],0)
+    pygame.draw.rect(screen,Brack,[size,667,400,25],0)
     img=pygame.image.load('car.jpg')
     img=pygame.transform.smoothscale(img,(52,50))
-    screen.blit(img,(B0[num],593))
+    screen.blit(img,(B0[num],573))
     pygame.display.update()
 def State_D():
     response3=urllib.request.urlopen(line0[num])
@@ -92,19 +93,34 @@ def Car(num):
         t.start()
 def GZ():
     button=text_0.render(('Prompt:fault,manual intervention is needed!'),1,Red)
-    screen.blit(button,(size,720))        
+    screen.blit(button,(size,690))
+def CGQ(num,x,y,a):
+    pygame.draw.rect(screen,Brack,[x+20,y+90,25,25],0)
+    button=text_2.render(text1[num],1,White)
+    screen.blit(button,(x-30,y+4))
+    img=pygame.image.load('cgq.jpg')
+    img=pygame.transform.smoothscale(img,(52,50))
+    screen.blit(img,(x,y+30))
+    button=text_0.render('{}'.format(a),1,White)
+    screen.blit(button,(x+20,y+90))
+    pygame.display.update()       
 if __name__ == '__main__':
     while True:
-        pygame.draw.rect(screen,Brack,[size,593,400,50],0)
-        pygame.draw.rect(screen,Brack,[size,687,400,25],0)
-        pygame.draw.rect(screen,Brack,[size,720,460,30],0)        
-        pygame.draw.rect(screen,Brack,[size+70,482,100,28],0)
-        pygame.draw.rect(screen,Green,[size+177,484,62,25],0)
+        time.sleep(1.5)
+        pygame.draw.rect(screen,Brack,[size,690,460,30],0)
+        pygame.draw.rect(screen,Brack,[size+70,382,100,28],0)
+        pygame.draw.rect(screen,Green,[size+177,384,62,25],0)
         button1=text_1.render("switch",1,Brack)
-        screen.blit(button1,(size+181,486))
+        screen.blit(button1,(size+181,386))
         button=text_1.render(button_text0,1,White)
-        screen.blit(button,(size+70,486))
-        B=[[0,size+187,66,button_text[0],color[0]],[1,size+187,112,button_text[1],color[1]],[2,size+187,158,button_text[2],color[2]],[3,size+187,204,button_text[3],color[3]],[4,size+187,250,button_text[4],color[4]],[5,size+187,296,button_text[5],color[5]],[6,size+187,342,button_text[6],color[6]],[7,size+187,388,button_text[7],color[7]],[8,size+187,434,button_text[8],color[8]]]
+        screen.blit(button,(size+70,386))
+        B=[[0,size+187,46,button_text[0],color[0]],[1,size+187,82,button_text[1],color[1]],[2,size+187,118,button_text[2],color[2]],[3,size+187,154,button_text[3],color[3]],[4,size+187,190,button_text[4],color[4]],[5,size+187,226,button_text[5],color[5]],[6,size+187,262,button_text[6],color[6]],[7,size+187,298,button_text[7],color[7]],[8,size+187,334,button_text[8],color[8]]]
+        response=urllib.request.urlopen('http://192.168.10.117:5000/status')
+        html=response.read().decode()
+        z=(html[1:-1].split(': '))[1][1:-1].split(', ')
+        D=[[0,size+52,420,z[0]],[1,size+177,420,z[1]],[2,size+302,420,z[2]]]
+        for k in D:
+            CGQ(k[0],k[1],k[2],k[3])
         for i in B:
             Process(i[0],i[1],i[2],i[3],i[4])
         if color0==[Green,Green,Green,Green,Green]:
@@ -120,9 +136,11 @@ if __name__ == '__main__':
                 if a not in list:
                     GZ()
             elif b[1]=='-1':
+                pygame.draw.rect(screen,Brack,[size,573,400,50],0)
+                pygame.draw.rect(screen,Brack,[size,667,400,25],0)
                 if a!=['1','1','1','1','1']:
                     GZ()
-            C=[[0,size+32,558,int(a[0]),color0[0]],[1,size+102,558,int(a[1]),color0[1]],[2,size+172,558,int(a[2]),color0[2]],[3,size+242,558,int(a[3]),color0[3]],[4,size+312,558,int(a[4]),color0[4]]]
+            C=[[0,size+32,538,int(a[0]),color0[0]],[1,size+102,538,int(a[1]),color0[1]],[2,size+172,538,int(a[2]),color0[2]],[3,size+242,538,int(a[3]),color0[3]],[4,size+312,538,int(a[4]),color0[4]]]
             for v in C:
                 Station(v[0],v[1],v[2],v[3],v[4])
         for event in pygame.event.get():
@@ -137,7 +155,7 @@ if __name__ == '__main__':
                 for index in range(len(pressed_array)):
                     if pressed_array[index]:
                         if index==0:
-                            if size+177<=pos[0]<=size+242 and 484<=pos[1]<=509:
+                            if size+177<=pos[0]<=size+242 and 384<=pos[1]<=409:
                                 if button_text0=="self-motion:" and button_text==["begin","begin","begin","begin","begin","begin","begin","begin","begin"]:
                                     button_text0="manual:"
                                     color=[Green,Green,Green,Green,Green,Green,Green,Green,Green]
@@ -161,7 +179,7 @@ if __name__ == '__main__':
                                         num=v[0]
                                         State_C(num)
                                         Car(num)
-                                        C=[[0,size+32,558,CGQ[v[0]][0],color0[0]],[1,size+102,558,CGQ[v[0]][1],color0[1]],[2,size+172,558,CGQ[v[0]][2],color0[2]],[3,size+242,558,CGQ[v[0]][3],color0[3]],[4,size+312,558,CGQ[v[0]][4],color0[4]]]
+                                        C=[[0,size+32,538,CGQ[v[0]][0],color0[0]],[1,size+102,538,CGQ[v[0]][1],color0[1]],[2,size+172,538,CGQ[v[0]][2],color0[2]],[3,size+242,538,CGQ[v[0]][3],color0[3]],[4,size+312,538,CGQ[v[0]][4],color0[4]]]
                                         for f in C:
                                             Station(f[0],f[1],f[2],f[3],f[4])
-    pygame.display.update()
+        pygame.display.update()
